e132082224cda06cf8dd1c1c92ca47115ff377cc
Kunal Kundu <kunal99kundu@gmail.com>
2025-02-16 03:30:36 +0530

    interpret queries for child segments

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use pest::Parser;
 #[allow(unused_imports)]
 use pest_derive::Parser;
+use serde_json::Value;
 
 #[derive(Parser)]
 #[grammar = "query.pest"]
@@ -12,7 +13,7 @@ struct Segment {
     selectors: Vec<Selector>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum SegmentType {
     Child,
     Descendant,
@@ -91,13 +92,45 @@ fn parse_to_segments(query: &str) -> Result<Vec<Segment>, Box<dyn std::error::Er
     Ok(normalized_segments)
 }
 
-pub fn interpret_query(
-    _input: serde_json::Value,
+fn process_segment(input: Vec<Value>, segment: &Segment) -> Vec<serde_json::Value> {
+    // todo: add support for descendant type
+    assert_eq!(segment.kind, SegmentType::Child);
+
+    input
+        .into_iter()
+        .flat_map(|item| {
+            segment
+                .selectors
+                .iter()
+                .flat_map(|selector| {
+                    match selector {
+                        Selector::Name(key) => item.get(key).cloned().into_iter().collect(),
+                        Selector::Index(idx) => {
+                            item.get(*idx as usize).cloned().into_iter().collect()
+                        } // todo: support -ve index
+                        Selector::Wildcard => match &item {
+                            Value::Array(values) => values.clone(),
+                            Value::Object(map) => map.values().cloned().collect(),
+                            _ => vec![],
+                        },
+                        Selector::Slice(_, _, _) => todo!(),
+                    }
+                })
+                .collect::<Vec<Value>>()
+        })
+        .collect()
+}
+
+pub fn interpret(
+    input_s: &str,
     query: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let segments = parse_to_segments(query);
-    println!("{segments:?}");
-    Ok(())
+) -> Result<Vec<serde_json::Value>, Box<dyn std::error::Error>> {
+    let segments = parse_to_segments(query)?;
+    if let Some(input) = serde_json::from_str(input_s)? {
+        Ok(segments.iter().fold(vec![input], process_segment))
+    } else {
+        Ok(vec![])
+    }
 }
 
 fn trim_quotes(input: &str) -> String {
@@ -202,5 +235,116 @@ mod tests {
         fails_to_parse!(invalid_escape_sequence, "$['\\q']");
         fails_to_parse!(unterminated_escape_sequence, "$['\\']");
         fails_to_parse!(unescaped_newline_in_quotes, "$[\"foo\nbar\"]");
+    }
+
+    mod interpret_queries {
+        use serde_json::json;
+
+        use super::*;
+
+        macro_rules! json_vec {
+            ([$($elem:expr),* $(,)?]) => {
+                vec![$(json!($elem)),*]
+            };
+        }
+
+        macro_rules! interprets_to {
+            ($name:ident, $input:expr, $query:expr, $expected:expr) => {
+                #[test]
+                fn $name() {
+                    let output = interpret($input, $query);
+                    assert!(output.is_ok());
+                    assert_eq!($expected, output.unwrap())
+                }
+            };
+
+            ($name:ident, $input:expr, $query:expr, $expected:expr, unordered) => {
+                #[test]
+                fn $name() {
+                    let output = interpret($input, $query);
+                    assert!(output.is_ok());
+
+                    let mut expected = $expected.clone();
+                    let mut result = output.unwrap();
+
+                    expected.sort_by_key(|v| serde_json::to_string(v).unwrap());
+                    result.sort_by_key(|v| serde_json::to_string(v).unwrap());
+
+                    assert_eq!(expected, result);
+                }
+            };
+        }
+
+        interprets_to!(only_root, r#"{"k": "v"}"#, "$", vec![json!({"k": "v"})]);
+
+        // named selector
+
+        const INPUT_1: &str = r#"{
+            "o": {"j j": {"k.k": 3}},
+            "'": {"@": 2}
+        }"#;
+
+        interprets_to!(
+            named_basic,
+            INPUT_1,
+            "$.o",
+            vec![json!({"j j": {"k.k": 3}})]
+        );
+
+        interprets_to!(named_nested, INPUT_1, "$.o['j j']", vec![json!({"k.k": 3})]);
+
+        interprets_to!(
+            named_nested_further,
+            INPUT_1,
+            "$.o['j j']['k.k']",
+            vec![json!(3)]
+        );
+
+        interprets_to!(
+            named_diff_delimiter,
+            INPUT_1,
+            r#"$.o["j j"]["k.k"]"#,
+            vec![json!(3)]
+        );
+
+        interprets_to!(
+            named_unusual_member_names,
+            INPUT_1,
+            r#"$["'"]["@"]"#,
+            vec![json!(2)]
+        );
+
+        interprets_to!(
+            named_multiple,
+            INPUT_1,
+            r#"$["'", "o"]"#,
+            vec![json!({"@": 2}), json!({"j j": { "k.k": 3 }})]
+        );
+
+        // wildcard selector
+
+        const INPUT_2: &str = r#"{
+            "o": {"j": 1, "k": 2},
+            "a": [5, 3]
+        }"#;
+
+        interprets_to!(
+            wildcard_first,
+            INPUT_2,
+            "$[*]",
+            vec![json!({"j": 1, "k": 2}), json!([5, 3])],
+            unordered
+        );
+
+        interprets_to!(wildcard_nested, INPUT_2, "$.o[*]", json_vec!([1, 2]));
+
+        interprets_to!(
+            wildcard_repeated,
+            INPUT_2,
+            "$.o[*, *]",
+            json_vec!([1, 2, 1, 2])
+        );
+
+        interprets_to!(wildcard_for_array, INPUT_2, "$.a[*]", json_vec!([5, 3]));
     }
 }
